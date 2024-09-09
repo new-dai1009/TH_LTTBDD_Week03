99cbdc11e4ea4f68d96dcc62ae37dfb531eb9bd3
H51M14\Student <Student@gmail.com>
2024-09-09 17:23:35 +0700

    Hoàn Thành FirstScreen

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <SafeAreaView style={styles.container}>
+            <StatusBar style="auto" />
+            <View style={styles.circle}>
+            </View>
+            <Text style={styles.text}>         GROW{"\n"}YOUR BUSINESS</Text>
+            <Text style={styles.texttow}>We will help to grow your business using {"\n"}                      online server</Text>
+            <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.buttonText}>
+                                    LOGIN
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.buttonText}>
+                                    SIGNUP
+                    </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#6EC2F7', 
+    },
+    circle: {
+        width: 200,
+        height: 200,
+        borderRadius: 100,
+        backgroundColor: '#6EC2F7', 
+        borderColor: 'black',
+        borderWidth: 8,
+    },
+    text:{
+        marginTop: 50,
+        justifyContent:'center',
+        fontSize:30,
+        fontWeight:'bold'
+
+    },
+    texttow:{
+        fontSize: 20,
+         marginTop: 30
+
+    },
+    buttonContainer:{
+        flexDirection:'row',
+        justifyContent:'space-around', 
+        width: '80%',
+        marginTop: 50 
+    },
+    buttonText:{
+        height:50,
+        width:100,
+        backgroundColor:'#CCCC33',
+        justifyContent:'center',
+        textAlign:'center',
+        borderRadius:20,
+        fontSize:20
+    }
 });
